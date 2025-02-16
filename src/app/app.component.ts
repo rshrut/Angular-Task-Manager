af2cb7f7f2300ee4d514c61b77b7b94aa21d1f9d
Sruthi R <rshruti451@gmail.com>
2025-02-16 23:42:43 +0530

    task manager done

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { TodolistComponent } from './todolist/todolist.component';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [TodolistComponent,FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Angular-Task-Manager';
+}
